cd833af49a9fc7b45f0abdfb6e4d3d5dd80ef832
Paulo Ragonha <paulo@ragonha.me>
2015-05-17 15:42:22 -0300

    fixes tests

--- spec/jenkins-dashboard.spec.jsx
+++ spec/jenkins-dashboard.spec.jsx
@@ -1,18 +1,17 @@
-var React = require('react');
-var TestUtils = React.addons.TestUtils;
-var JenkinsDashboard = require('../lib/jenkins-dashboard.jsx');
+import React from 'react/addons';
+import JenkinsDashboard from '../lib/jenkins-dashboard.jsx';
 
 
-describe("JenkinsDashboard", function() {
+describe('JenkinsDashboard', function() {
   var component;
 
   beforeEach(function() {
-    component = TestUtils.renderIntoDocument(
-      <JenkinsDashboard name="Jonh"/>
+    component = React.addons.TestUtils.renderIntoDocument(
+      <JenkinsDashboard name='Jonh'/>
     );
   });
 
-  it("should render", function() {
+  it('should render', function() {
     expect(component.getDOMNode().className).toEqual('jenkins-dashboard');
   });
 });
