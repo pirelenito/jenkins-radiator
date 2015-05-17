cb54338316389f5a0926b67e12ede20d60ed766a
Paulo Ragonha <paulo@ragonha.me>
2015-05-17 18:07:41 -0300

    installs babel-runtime to be able to use ES7 async/await

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,11 @@ module.exports = {
       {
         test: /(\.js)|(\.jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        query: {
+          optional: ['runtime'],
+          stage: 0
+        }
       }
     ]
   }
